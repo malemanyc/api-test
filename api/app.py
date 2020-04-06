84f39815263e386ead7aee9ba6aa5e9adef021c9
Mercè Alemany <malemanychavarria@gmail.com>
2020-04-06 03:00:04 +0200

    Add some documentation

--- api/app.py
+++ api/app.py
@@ -8,21 +8,30 @@ from flask import Flask, jsonify, request, g
 # Createan a Flask instance
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST']) #converts the index method into an url endpoint?
+@app.route('/', methods=['GET', 'POST']) 
 def index():
-    """Present documentation. """
+    """ 
+    GET:Present documentation.
+    POST: Send some raw string and obtain json format of all names contained.
+    """
+
     if request.method == 'POST':
+        # Capture raw input string
         input_string = request.data
+
+        # Process input string
         data = parsestring.process_str(str(input_string))
+
+        # Return  json data
         return jsonify(data), 201
+
+    # When request method is GET
     else:
+
+        # Open documentation file
         with open(os.path.dirname(app.root_path) + '/README.md', 'rt') as doc_file:
             # Read documentation file
             content = doc_file.read()
+
             # Convert to HTML
             return markdown.markdown(content)
-
-
-@app.route('/test1/<int:num>', methods=['GET']) # if you don't specify method, default is GET
-def get_multiply10(num):
-	return jsonify({'result': num*10})
